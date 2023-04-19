3d98eb9e581e243d4aafc586ed8a316f72db7e4f
Johnathon Melton <seventh.acoustic@gmail.com>
2023-04-19 12:40:17 -0500

    First C# program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("It's Monday Morning. Wish it was Saturday!");
+        }
+    }
+}
